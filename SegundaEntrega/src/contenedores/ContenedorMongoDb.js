7a2a6b03501aacc3331074f56427ca53dfa2b4e2
leslieMondragon <iscmond@gmail.com>
2022-09-06 23:11:42 -0500

    update

---
+++ SegundaEntrega/src/contenedores/ContenedorMongoDb.js
@@ -0,0 +1,69 @@
+export default class MongoContainer {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async add(object) {
+        try {
+            let elementCreated = await this.model.create(object);
+            return elementCreated._id;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getAll() {
+        try {
+            let elements = await this.model.find({});
+            let result = elements.map(x => {
+                return {
+                    id: x._id.toString(),
+                    ...x.toObject()
+                }
+            });
+
+            return result;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async getById(id) {
+        try {
+            let element = await this.model.findOne({
+                _id: id
+            }, {
+                __v: 0
+            });
+            return {
+                id: element._id,
+                ...element.toObject()
+            };
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async updateById(id, objectToUpdate) {
+        try {
+            return await this.model.updateOne({
+                _id: id
+            }, {
+                $set: objectToUpdate
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteById(id) {
+        try {
+            return await this.model.deleteOne({
+                _id: id
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
